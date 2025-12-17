d19035f45b742343773c680a140331556ae0f421
Althaf671 <althafmain@gmail.com>
2025-12-17 20:37:46 +0700

    Git: Switch back to dev environment

--- src/app/assets/icons/common/styles/icon.style.default.ts
+++ src/app/assets/icons/common/styles/icon.style.default.ts
@@ -2,4 +2,5 @@ export const ICON_STYLE_DEFAULT = {
     colorDefault: 'var(--color-icon)',
     hugeStyleDefault: { 'font-size': '24px', 'font-weight': '500' },
     lucideStyleDefault: { size: 24, strokeWidth: 1.5, }
-} as const;
+} as const
+        
